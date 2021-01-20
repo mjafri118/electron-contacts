46bb07dfbcde7eb17b0ca37c471c3598f447ba92
Mohib Jafri <mjafri@Mohibs-MacBook-Pro.local>
2021-01-19 17:16:19 -0800

    node-mac-contacts commit
    Steps after boilerplate was successfully running:
    
    1. Add Node-Mac-Contacts to ./src/node_modules:
    
    - yarn add node-mac-contacts
    - yarn postinstall
    
    2. Rebuild at top level ./ too
    - yarn postinstall

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/icon.svg';
+const { getAllContacts, getAuthStatus } = require('node-mac-contacts')
+
 
 const Hello = () => {
+  const contacts = getAllContacts()
+  const demoText = contacts.length > 0 ? contacts[0].firstName : "Couldn't read any contacts."
   return (
     <div>
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
+      <h2>Access to Contacts: {getAuthStatus()}</h2>
+      <h2>First Contact Sample: {demoText}</h2>
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
